4c74e012768da7bb7076cfdbd4214c9ddd1dc050
João Victor <skai7ami@gmail.com>
2025-08-04 18:01:10 -0300

    feat: create sidebar for main layout

--- Prism.Presentation/Program.cs
+++ Prism.Presentation/Program.cs
@@ -3,8 +3,7 @@ using Prism.Presentation.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -18,11 +17,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();
