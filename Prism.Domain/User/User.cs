f223df2dad2a4918ebc1c30b5110fb5885ea9420
João Victor <skai7ami@gmail.com>
2025-08-05 08:09:58 -0300

    feat: create user domain

---
+++ Prism.Domain/User/User.cs
@@ -0,0 +1,26 @@
+namespace Prism.Domain.User;
+
+public class User
+{
+    public Guid Guid { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public bool IsBlocked { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+
+    public User(string name, string email, string password)
+    {
+        Guid = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        Password = password;
+        IsBlocked = false;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    // EF ctor
+    private User() { }
+}
